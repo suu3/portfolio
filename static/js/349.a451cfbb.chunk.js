ac5e1939e23bfdc5ab3e44ab67482f5dded018d9
suu3 <ahob789@ewhain.net>
2022-06-13 02:00:36 +0900

    Updates

--- static/js/349.a451cfbb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkknockknock=self.webpackChunkknockknock||[]).push([[349],{349:function(k,n,c){c.r(n);c(791);var u=c(184);n.default=function(k){return(0,u.jsx)("div",{children:"Buddy"})}}}]);
-//# sourceMappingURL=349.a451cfbb.chunk.js.map
--- static/js/775.9941b299.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkknockknock=self.webpackChunkknockknock||[]).push([[775],{775:function(n,r,c){c.r(r),c.d(r,{default:function(){return i}});c(791);var e="error_error__Gi2KW",s=c(403),k=c(184),i=function(n){return(0,k.jsxs)("section",{className:e,children:[(0,k.jsx)("img",{src:s.Y.NOT_FOUND,alt:"404"}),(0,k.jsx)("div",{children:"\ud398\uc774\uc9c0\uac00 \uc874\uc7ac\ud558\uc9c0 \uc54a\uc2b5\ub2c8\ub2e4."})]})}}}]);
-//# sourceMappingURL=775.9941b299.chunk.js.map
