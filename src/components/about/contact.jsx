adc665dadf3cbf6913c3fd78a17ebd5666f74d62
suu3 <ahob789@ewhain.net>
2022-06-12 21:27:21 +0900

    [FEAT] About 마무리

--- src/components/about/contact.jsx
+++ src/components/about/contact.jsx
@@ -4,16 +4,16 @@ import styles from "styles/about/index.module.css";
 const Contact = (props) => {
   return (
     <article className={styles[`contact`]}>
-      <p>
+      <p data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
         <strong>Tel</strong> 010-2674-1080
       </p>
-      <p>
+      <p data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
         <strong>E-mail</strong> ahob789@ewhain.net
       </p>
-      <p>
+      <p data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
         <strong>Github</strong> https://github.com/suu3
       </p>
-      <p>
+      <p data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
         <strong>Tech Blog</strong> https://temp-ha3.tistory.com
       </p>
     </article>
