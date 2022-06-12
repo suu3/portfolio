e26320d697634c01f0f20feb2da1a3aa9612bcd4
suu3 <ahob789@ewhain.net>
2022-06-13 05:07:12 +0900

    Updates

--- static/js/349.fc90293a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkknockknock=self.webpackChunkknockknock||[]).push([[349],{1349:function(k,n,c){c.r(n);c(2791);var u=c(184);n.default=function(k){return(0,u.jsx)("div",{children:"Buddy"})}}}]);
-//# sourceMappingURL=349.fc90293a.chunk.js.map
