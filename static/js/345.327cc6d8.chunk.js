eb8386ffa961a13826c8083f537639a38671ad00
suu3 <ahob789@ewhain.net>
2023-09-20 03:48:42 +0900

    Updates

---
+++ static/js/345.327cc6d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio_template=self.webpackChunkportfolio_template||[]).push([[345],{856:function(t,n,a){a.d(n,{pUp:function(){return c}});var e=a(983);function c(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M20 18c1.1 0 1.99-.9 1.99-2L22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2H0v2h24v-2h-4zM4 6h16v10H4V6z"}}]})(t)}},655:function(){}}]);
+//# sourceMappingURL=345.327cc6d8.chunk.js.map
